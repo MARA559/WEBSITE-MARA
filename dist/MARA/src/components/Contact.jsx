0e629d1df23f306f748302c5ef185ccacdbb29d2
MARA559 <fmara24@beder.edu.al>
2025-05-25 14:42:31 +0200

    Fixed filename issue for Netlify

--- dist/MARA/src/components/Contact.jsx
+++ dist/MARA/src/components/Contact.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import emailjs from "@emailjs/browser";
 function Contact() {
+
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_8kfmurh",
+    "template_ecbx33i",
+    e.target,
+    "2rVMjAYWwsWKi5CnJ"
+  ).then((result) => {
+    console.log("✅ Email sent successfully!", result.text);
+    alert("✅ Your message has been sent!");
+  }).catch((error) => {
+    console.error("❌ Error sending email:", error);
+    alert("❌ There was an error sending your message.");
+  });
+};
   return (
     <div className="py-10">
       <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
-      <form className="max-w-lg mx-auto">
+    <form className="max-w-lg mx-auto" onSubmit={sendEmail}>
         <div className="mb-4">
           <label htmlFor="name" className="block text-sm font-medium mb-2">Name</label>
           <input type="text" id="name" className="w-full p-2 border border-gray-300 rounded" required />
